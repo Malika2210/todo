90eb54a52c2c36b490dcda856052c8045c697a39
Your Name <you@example.com>
2021-01-25 00:38:06 +0600

    added todo form

--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from .models import ToDo
 
 # Create your views here.
@@ -16,3 +16,11 @@ def second(request):
 
 def third(request):
     return HttpResponse("This is third page")
+
+
+def add_todo(request):
+    form = request.POST
+    text = form["todo_text"]
+    todo = ToDo(text=text)
+    todo.save()
+    return redirect(test)
